49d482bdcb64d6ac6ca68ba9559431c7defa48eb
Andrew Mokin <parkovka.mokin@yandex.ru>
2023-01-27 21:28:27 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, кот. на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+Console.WriteLine("Введите два числа: ");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Является ли первое число квадратом второго?");
+
+if (num1 == num2 * num2)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
