6eaf348ab26bd8e3d8fd07312aff0f353c2e84b7
Andrew Mokin <parkovka.mokin@yandex.ru>
2023-02-01 06:37:35 +0300

    Webinar2

---
+++ Task00/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, кот. выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру.
+
+Console.Clear();
+int Num = new Random().Next(10, 100);
+int FirstNum = Num / 10;
+int SecondNum = Num % 10;
+
+Console.Write($"Наибольшая цифра числа {Num}: ");
+
+if (FirstNum >= SecondNum) Console.WriteLine(FirstNum);
+else Console.WriteLine(SecondNum);
