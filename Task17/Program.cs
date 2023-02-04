b4742cf6acaa0819cdd3d82968cd43d7605b3d60
Andrew Mokin <parkovka.mokin@yandex.ru>
2023-02-04 12:11:36 +0300

    Webinar3

---
+++ Task17/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, кот. принимает на вход координаты точки (X и Y). Причем обе координаты не равны нулю.
+// И выдает номер четверти плоскости в которой они находятся.
+
+Console.Clear();
+Console.Write("Введите координату X: ");
+int X = int.Parse(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+int Y = int.Parse(Console.ReadLine());
+
+if (X > 0 && Y > 0) Console.WriteLine("1я четверть");
+else if (X < 0 && Y > 0) Console.WriteLine("2я четверть"); // в данном случае можно без else if, так как нет пересечений условий
+else if (X < 0 && Y < 0) Console.WriteLine("3я четверть");
+else if (X > 0 && Y < 0) Console.WriteLine("4я четверть");
+else Console.WriteLine("Введите отличную от нуля координату");
---
+++ Task22/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int Count = 1;
+
+while (Count <= Math.Abs(N))
+{
+    if (Count == N)
+    {
+        Console.Write($"{Math.Pow(Count, 2)}.");
+    }
+    else
+    {
+        Console.Write($"{Math.Pow(Count, 2)}, ");
+    }
+    Count++;
+}
