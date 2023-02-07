9bb64743d0cefd8796ff74e7d78e35810ae6c785
Andrew Mokin <parkovka.mokin@yandex.ru>
2023-02-07 22:05:46 +0300

    order commit

---
+++ Task005/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, кот. на вход принимает одно число (N), а на выходе показывает 
+// все целые числа в промежутке от -N до N.
+Console.Clear();
+Console.WriteLine("Введите целое число: ");
+int UserNum = int.Parse(Console.ReadLine());
+int Digit = -UserNum;
+Console.Write("Интервал: ");
+
+while (Digit <= UserNum)
+{
+    Console.Write(Digit + " ");
+    Digit++;
+}
---
+++ Task024/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, кот. принимает на вход число (А) и выдает сумму чисел от 1 до А.
+
+int GetSumNum(int Num)
+{
+    int Sum = 0;
+    for (int i = 0; i <= Num; i++)
+    {
+        Sum += i;
+    }
+    return Sum;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр = {GetSumNum(A)}");
