f1700563e934aa2736784b59e3b4430293896b88
Andrew Mokin <parkovka.mokin@yandex.ru>
2023-02-07 22:09:30 +0300

    deleted old directory

--- Task0/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// Напишите программу, которая на вход принимает число и выдает квадрат (число умноженное на само себя).
-Console.Clear();
-Console.WriteLine("Введите число ");
-int num = int.Parse(Console.ReadLine()); // конвертация string в int
-int sqre = num * num;
-Console.WriteLine("Квадрат числа " + num + " равен " + sqre);
--- Task11/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// Напишите программу, кот. выводит случайное трехзначное число и удаляет вторую цифру этого числа.
-
-Console.Clear();
-int Num = new Random().Next(100, 1000);
-int FirstNum = Num / 100;
-int ThirdNum = Num % 10;
-int Result = FirstNum * 10 + ThirdNum;
-
-Console.WriteLine($"{Num}, {Result}");
