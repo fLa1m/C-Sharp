e4706813106d708f1f8f37ef7572f2a301f0187f
Andrey Mokin <parkovka.mokin@yandex.ru>
2023-02-14 21:25:27 +0300

    Task 042

--- Task042/Program.cs
+++ Task042/Program.cs
@@ -1,1 +1,36 @@
 ﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+int DataEntry(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int ArrayLength(int num)
+{
+    int count = 0;
+    while (num != 0)
+    {
+        num /= 2;
+        count++;
+    }
+    return count;
+}
+
+int[] GenerateArray(int length, int num)
+{
+    int[] array = new int[length];
+    for (int i = length - 1; i >= 0; i--) 
+    {
+        array[i] = num % 2;
+        num /= 2;
+    }
+    return array;
+}
+
+Console.Clear();
+int number = DataEntry("Введите число: ");
+//int length = ArrayLength(number);
+int [] arr = GenerateArray(ArrayLength(number), number);
+Console.WriteLine(String.Join("", arr));
