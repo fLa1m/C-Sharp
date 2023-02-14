71f8eec9aa2d6b378c50231a5be6908207172e6b
Andrew Mokin <parkovka.mokin@yandex.ru>
2023-02-14 23:21:08 +0300

    Task 044 & Task 045

---
+++ Task044/Program.cs
@@ -0,0 +1,27 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+int DataEntry(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int[] Fibonacci(int num)
+{
+    int[] array = new int[num];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < num; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+
+Console.Clear();
+int number = DataEntry("Введите количество чисел Фибонначи: ");
+Console.WriteLine($"{String.Join(" ", Fibonacci(number))}");
---
+++ Task045/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного
+// копирования. 
+
+int DataEntry(string message)
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int[] GenerateArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+int[] CopyArray(int[] array)
+{
+    int[] arr = new int[array.Length];
+    int i = 0;
+    foreach (int el in array)
+    {
+        arr[i] = el;
+        i++;
+    }
+    return arr;
+}
+
+Console.Clear();
+int length = DataEntry("Введите длину массива: ");
+int min = DataEntry("Введите минимальное значение интервала: ");
+int max = DataEntry("Введите максимальное значение интервала: ");
+int[] array = GenerateArray(length, min, max);
+Console.WriteLine("Исходный массив:");
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int[] newArray = CopyArray(array);
+Console.WriteLine("Скопированный массив:");
+Console.WriteLine($"[{String.Join(", ", newArray)}]");
